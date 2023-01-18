9ae9d77a8bb8d0b06883e5db87756f625941caa8
DiscordRPC <lukashammerprivate@gmail.com>
2023-01-18 19:46:05 +0000

    chore: update module exports

---
+++ src/index.ts
@@ -0,0 +1,8 @@
+// Logger
+export * from './lib/utils/logger/LogManager';
+export * from './lib/utils/logger/Logger';
+export * from './lib/utils/logger/LogLevel';
+
+// Exproos
+export * from './lib/Exproos';
+export * from './lib/structures/Middleware';
